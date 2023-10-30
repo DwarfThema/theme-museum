388cb7b8fa70c2b95219eeb596c4b24ab701c36d
Junho Park <dwarfthema@gmail.com>
2023-10-30 16:58:22 +0900

    jeongeun words

--- app/pages/jeongeunPage.tsx
+++ app/pages/jeongeunPage.tsx
@@ -23,13 +23,13 @@ const JeongeunPage: React.FC<ContentsProps> = ({ onMainLoading }) => {
         </div>
         <div className="xl:w-[782px] lg:w-fit  height650:hidden  xl:mt-7 lg:mt-3 lg:block zero:hidden ">
           <div className="text-base font-extralight">
-            {`2 EURO 프로젝트는 여행을 다니며 남은 동전에서 시작되었다. 여행중에 성당안에 들어가서 동전하나를 넣고 소원빌던 기억이 있다. 그 순간을 포착하여 집으로 가져오고싶은 마음에 성당안의 천사들을 접어서 숨겨 집으로 데려왔다. 집에서 동전을 구멍에 놓고 소원을 비니 그 순간 불이 켜지며 천사들이 문을 닫아버렸다.`}
+            {`<2 EURO PROJEKT> 는 여행을 다니며 남은 동전에서 시작되었다. 여행중에 성당안에 들어가서 동전 하나를 넣고 소원을 빌던 기억이 있다. 그 순간을 포착하여 집으로 가져오고싶은 마음에 성당안의 천사들을 접어서 숨겨 집으로 데려왔다. 집에서 동전을 구멍에 놓고 소원을 비니 그 순간 불이 켜지며 천사들이 문을 닫아버렸다.`}
           </div>
           <div className="mt-7 font-semibold text-xl xl:block lg:hidden height750:hidden">
             DIGITFORMING
           </div>
           <div className="mt-1 xl:block lg:hidden height750:hidden">
-            {`열려있던 구조물은 동전으로 완성되며 닫힌다. 실제로 움직이는 모습을 인터렉션을 통해 작업의 개연성과 과정에 생동감을 불어넣을 수 있다.`}
+            {`웹 아트로 실제 움직임을 구현하면서 작업의 개연성과 과정에 생동감을 더한다. 사용자들은 열려있던 구조물이 동전으로 완성되고 열쇠로 닫히는 엔딩을 관람하며 실물 작업을 체험해볼 수 있다.`}
           </div>
         </div>
         <div className="xl:mt-7 lg:mt-0 zero:mt-0">
